9ade3e0776afee4140f486f121a4595668fd70e2
Ben Singer <ben.singer@sds.co.uk>
2022-07-13 14:37:47 +0100

    button changes hover before after

--- src/components/skills-ui/skills-ui-header-tabs/SkillsUIHeaderTabItem.jsx
+++ src/components/skills-ui/skills-ui-header-tabs/SkillsUIHeaderTabItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SkillsUIHeaderTabItem = ({name}) => {
   return (
-    <button className='px-8 py-4 font-bold text-md relative bg-brand-secondary hover:bg-brand-primary hover:underline focus:bg-brand-primary rounded-t-xl transition-colors before:-left-2 before:bottom-0 before:absolute before:bg-brand-alt before:h-3 before:w-3 after:-right-2 after:bottom-0 after:absolute after:bg-brand-alt after:h-3 after:w-3 hover:before:bg-brand-primary before:transition-colors hover:after:bg-brand-primary' role="tab">
+    <button className='px-8 py-4 font-bold text-md relative bg-brand-secondary hover:bg-brand-primary hover:underline focus:bg-brand-primary rounded-t-xl focus:underline transition-colors before:-left-2 before:bottom-0 before:absolute before:bg-brand-secondary before:h-3 before:w-3 after:-right-2 after:bottom-0 after:absolute after:bg-brand-secondary after:h-3 after:w-3 hover:before:bg-brand-primary before:transition-colors hover:after:bg-brand-primary' role="tab">
       {name}
     </button>
   )
