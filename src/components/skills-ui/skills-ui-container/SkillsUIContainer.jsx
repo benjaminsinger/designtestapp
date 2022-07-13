d7e22764926e7a0a9d2d4cd059e3431b605c81bc
Ben Singer <ben.singer@sds.co.uk>
2022-07-13 15:13:11 +0100

    skills ui subnav

--- src/components/skills-ui/skills-ui-container/SkillsUIContainer.jsx
+++ src/components/skills-ui/skills-ui-container/SkillsUIContainer.jsx
@@ -1,12 +1,12 @@
 import SkillsUIHeader from '../skills-ui-header/SkillsUIHeader';
+import SkillsUISubnav from '../skills-ui-subnav/SkillsUISubnav';
 
-const SkillsUIContainer = props => {
+const SkillsUIContainer = () => {
   return (
-    <>
-      <div className='text-white mx-auto pb-5 bg-brand-primary max-w-screen-xl rounded-t-xl overflow-hidden'>
-        <SkillsUIHeader />
-      </div>
-    </>
+    <div className='text-white mx-auto bg-brand-primary max-w-screen rounded-t-xl overflow-hidden'>
+      <SkillsUIHeader />
+      <SkillsUISubnav />
+    </div>
   )
 }
 
