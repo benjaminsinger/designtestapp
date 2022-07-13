9a1f3cbab3074f2ba6ac05da9849f79397a45dce
Matt Skelton <matt.skelton@sds.co.uk>
2022-07-13 14:31:51 +0100

    Added next steps block

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Hero } from "./components/hero/Hero";
+import { NextSteps } from "./components/next-steps/NextSteps";
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <NextSteps />
     </div>
   );
 }
