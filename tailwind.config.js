73b9ecb6ab73a38a85498749dcc5fc3440fb5413
Ben Singer <ben.singer@sds.co.uk>
2022-07-13 10:02:28 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [
+    require('@benjaminsinger/sds-tailwind-config').base
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+}
