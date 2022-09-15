53e13667e325e32be856146efee419d214b72a5e
Matt Skelton <matt.skelton@sds.co.uk>
2022-09-15 11:03:53 +0100

    Initial commit

--- src/components/skills-ui/skills-ui-container/SkillsUIContainer.jsx
+++ src/components/skills-ui/skills-ui-container/SkillsUIContainer.jsx
@@ -4,13 +4,15 @@ import SkillsUISubnav from "../skills-ui-subnav/SkillsUISubnav";
 
 const SkillsUIContainer = () => {
   return (
-    <div className='text-white mx-auto bg-brand-primary max-w-screen rounded-t-xl overflow-hidden container my-10'>
+    <div className="text-white mx-auto bg-brand-primary max-w-screen rounded-t-xl overflow-hidden container">
       <SkillsUIHeader />
       <div class="px-10 py-5 gap-4 flex flex-wrap">
         <SkillsUISubnav />
         <div class="flex w-full gap-4">
-          <div className='w-1/2 bg-brand-alt'></div>
-          <div className='w-1/2 bg-brand-alt'><SkillsCard /></div>
+          <div className="w-1/2 bg-brand-alt"></div>
+          <div className="w-1/2 bg-brand-alt">
+            <SkillsCard />
+          </div>
         </div>
       </div>
     </div>
